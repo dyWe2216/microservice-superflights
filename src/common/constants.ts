3c9d06f41b38c36335aaf747ae138c7ea85463c1
dyWe2216 <dnleodud@gmail.com>
2023-01-05 08:38:36 +0900

    add: add rabbitmq module

---
+++ src/common/constants.ts
@@ -0,0 +1,3 @@
+export enum RabbitMQ {
+  UserQueue = 'users',
+}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ProxyModule } from 'src/common/proxy/proxy.module';
 import { UserController } from './user.controller';
 
 @Module({
-  controllers: [UserController]
+  imports: [ProxyModule],
+  controllers: [UserController],
 })
 export class UserModule {}
