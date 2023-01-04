7d997a47ce0b3ecae56551d8ebcd9a1aef4f7aaa
dyWe2216 <dnleodud@gmail.com>
2023-01-05 08:27:03 +0900

    add: user module added (controller, dto)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { AppService } from './app.service';
       envFilePath: ['.env.development'],
       isGlobal: true,
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/user/user.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('api/v2/user')
+export class UserController {}
