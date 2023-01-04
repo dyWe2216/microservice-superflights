4f70b8946dd5cbc393ecce1c166da9236c3a933c
dyWe2216 <dnleodud@gmail.com>
2023-01-05 08:19:35 +0900

    add: init commit

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { AllExceptionFilter } from './common/filters/http-exception.filter';
+import { TimeOutInterceptor } from './common/interceptors/timeout.interceptor';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new AllExceptionFilter());
+  app.useGlobalInterceptors(new TimeOutInterceptor());
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
